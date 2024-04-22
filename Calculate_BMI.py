5f378127ed0280e80d0114db0dd93e3a912235f6
Radim Kubela <radim.kubela@gmail.com>
2024-04-22 06:18:09 +0200

    Inputy přepsány do angličtiny

--- Calculate_BMI.py
+++ Calculate_BMI.py
@@ -8,8 +8,8 @@
 
 # if bmi > 30 return "Obese"
 
-weight = input("Zadejte svoji váhu v kg: ")
-height = input("Zadejte svoji výšku v m: ")
+weight = input("Put your weight in kilograms: ")
+height = input("Put your height in meters: ")
 
 def bmi(weight, height):
     bmi = float(weight) / (float(height) ** 2)
@@ -24,15 +24,3 @@ def bmi(weight, height):
     
 print(bmi(weight, height))
     
-#   BMI = float(Váha) / (float(Výška)**2)
-
-# print("Vaše BMI je", BMI)
-
-# if BMI < 18.5:
-#   print("Máte podváhu")
-# elif BMI >= 18.5 and BMI < 25:
-#   print("Máte ideální váhu")
-# elif BMI >= 25 and BMI < 30:
-#   print("Máte nadváhu")
-# else:
-#   print("Jste obézní")
